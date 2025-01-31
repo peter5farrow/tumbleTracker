4b547171538475b424e3acce2bb50ca6c0802ac2
Peter Farrow <pnfarrow@gmail.com>
2025-01-30 20:26:29 -0700

    fixed add levels to day function for mongodb

--- server/TESING.js
+++ server/TESING.js
@@ -1,4 +1,4 @@
-import { Level, Event, Day, Time, Coach, db } from "./model.js";
+import { Level, Event, Day, Coach, db } from "./model.js";
 import express from "express";
 import dotenv from "dotenv";
 
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 // GET
-app.get("/days", async (req, res) => {
+app.get("/api/days", async (req, res) => {
   try {
     const days = await Day.find();
     res.json(days);
@@ -24,7 +24,7 @@ app.get("/days", async (req, res) => {
   }
 });
 
-app.get("/levels", async (req, res) => {
+app.get("/api/levels", async (req, res) => {
   try {
     const levels = await Level.find();
     res.json(levels);
@@ -33,7 +33,7 @@ app.get("/levels", async (req, res) => {
   }
 });
 
-app.get("/events", async (req, res) => {
+app.get("/api/events", async (req, res) => {
   try {
     const events = await Event.find();
     res.json(events);
@@ -42,7 +42,7 @@ app.get("/events", async (req, res) => {
   }
 });
 
-app.get("/coaches", async (req, res) => {
+app.get("/api/coaches", async (req, res) => {
   try {
     const coaches = await Coach.find();
     res.json(coaches);
@@ -52,13 +52,80 @@ app.get("/coaches", async (req, res) => {
 });
 
 // POST new day
-app.post("/days", async (req, res) => {
-  const { dayCode, dayName, levels } = req.body;
-  const newDay = new Day({ dayCode, dayName, levels });
+// app.post("/api/days", async (req, res) => {
+//   const { dayCode, dayName, levels } = req.body;
+//   const newDay = new Day({ dayCode, dayName, levels });
 
+//   try {
+//     const savedDay = await newDay.save();
+//     res.status(201).json(savedDay);
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
+//   }
+// });
+
+// PUT
+app.put("/api/add-event", async (req, res) => {
+  const { day, level, event, startTime, duration } = req.body;
+
+  const thisDay = Day.find({ dayCode: day });
+
+  const timeKeys = Object.keys(thisDay[level]["times"]);
+  const startIndex = timeKeys.indexOf(startTime);
+  let hasConflict = false;
+
+  for (const eachLevel in thisDay) {
+    for (
+      let i = startIndex;
+      i < startIndex + duration / 5 && i < timeKeys.length;
+      i++
+    ) {
+      const key = timeKeys[i];
+      if (day[eachLevel][key] === event) {
+        hasConflict = true;
+      }
+    }
+  }
+  if (hasConflict) {
+    res.status(409).json(thisDay);
+  } else {
+    for (
+      let i = startIndex;
+      i < startIndex + duration / 5 && i < timeKeys.length;
+      i++
+    ) {
+      const key = timeKeys[i];
+      day[level][key] = event;
+    }
+  }
+  const updatedDay = await thisDay.save();
+  res.status(201).json(updatedDay);
+});
+
+app.put("/api/add-levels", async (req, res) => {
+  const { day, levels } = req.body;
+
+  const levelsArr = [];
+  for (const level of levels) {
+    levelsArr.push(await Level.find({ levelCode: level }));
+  }
+  console.log(levelsArr);
   try {
-    const savedDay = await newDay.save();
-    res.status(201).json(savedDay);
+    const thisDay = await Day.findOne({ dayCode: day });
+    console.log(thisDay);
+
+    if (!thisDay.levels) {
+      thisDay.levels = levelsArr;
+      const savedDay = await thisDay.save();
+      res.status(201).json(savedDay);
+    } else {
+      const thisDayLevels = [...thisDay.levels];
+      const allLevels = thisDayLevels.concat(levelsArr);
+      thisDay.levels = allLevels;
+      const savedDay = await thisDay.save();
+
+      res.status(201).json(savedDay);
+    }
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
